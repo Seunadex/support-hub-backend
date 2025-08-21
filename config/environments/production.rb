f6af849d26b059c24c368f827c7910eae468a1d0
Seun Adekunle <adekunleseun001@gmail.com>
2025-08-21 15:43:33 +0100

    fix entrypoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "example.com" }
-  config.x.frontend_base_url = ENV.fetch("FRONTEND_BASE_URL")
+  config.x.frontend_base_url = "https://support-hub-frontend.pages.dev"
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
